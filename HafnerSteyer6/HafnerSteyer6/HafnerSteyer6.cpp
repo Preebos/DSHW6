7f8162e6ea9b65211d6b86c452613f09d12ffc87
Robin Steyer <robinmarie051@yahoo.com>
2015-07-28 22:19:13 -0400

    changed arrays to vectors and added helper functions

--- HafnerSteyer6/HafnerSteyer6/HafnerSteyer6.cpp
+++ HafnerSteyer6/HafnerSteyer6/HafnerSteyer6.cpp
@@ -3,22 +3,26 @@
 // Course Number: CS2028
 // Instructor: Anca Ralescu
 // TA: Suryadip Chakraborty
-// Abstract: Assignment 4: This application serves to display the effectiveness of
-//	various sorting algorithms. Run the program for a better description.
+// Abstract: Assignment 5: This application tests different collision resolution methods
+	// in hash tables of various sizes. The user determines the entried and load ratio.
 // Preconditions: ?
 // Postconditions: ?
 
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <vector>
 
 using namespace std;
 
 enum collisionResolutionType {LINEAR_PROBING, QUADRATIC_PROBING, DOUBLE_HASHING, CHAINING};
+const int SIZE = 23;
 
-void addToHashTable(int hashTable[], int value, collisionResolutionType crType) {
+void addToHashTable(vector<int> &hashTable, int value, collisionResolutionType crType) {
+	int index = 0;
 	if (crType == LINEAR_PROBING) {
-
+		index = value % SIZE;
+		hashTable[index] = value;
 	}
 	
 	else if (crType == QUADRATIC_PROBING) {
@@ -36,17 +40,43 @@ void addToHashTable(int hashTable[], int value, collisionResolutionType crType) 
 	return;
 }
 
+int numOfEntries(vector<int> hashTable) {
+	int count = 0;
+	for (unsigned int n = 0; n < hashTable.size(); n++) {
+		if (hashTable[n] != 0) {
+			count++;
+		}
+	}
+	return count;
+}
+
+
+void printHashTable(vector<int> hashTable) {
+	cout << "Index: \tEntry: \n";
+	for (unsigned int n = 0; n < hashTable.size(); n++) {
+		if (hashTable[n] != 0) {
+			cout << n << "\t" << hashTable[n] << endl;
+		}
+		else {
+			cout << n << "\t" << "----" << endl;
+		}
+	}
+}
+
 int main() {
-	const int size = 23;
-	int hashTable1[size];
-	int hashTable2[size];
-	int hashTable3[size];
-	int hashTable4[size];
+	vector<int> hashTable1;
+	vector<int> hashTable2;
+	vector<int> hashTable3;
+	vector<int> hashTable4;
 	bool hashTableFull = false;
 	int value;
 
+	for (int n = 0; n < SIZE; n++) {
+		hashTable1.push_back(0);
+	}
+
 	double loadRatio = 0;
-	cout << "This application will insert values into a hash table of size " << size << ".\n\n";
+	cout << "This application will insert values into a hash table of size " << SIZE << ".\n\n";
 	cout << "What load ratio would you like to use? ";
 	cout << "(enter a decimal representation of a percentage, e.g. 0.2 or 0.55)\n";
 	cin >> loadRatio;
@@ -59,7 +89,12 @@ int main() {
 	while (!hashTableFull) {
 		cin >> value;
 		addToHashTable(hashTable1, value, LINEAR_PROBING);
+		if ((double)numOfEntries(hashTable1) / (double)SIZE >= loadRatio) {
+			hashTableFull = true;
+		}
 	}
+
+	printHashTable(hashTable1);
 
 	system("pause");
 	return 0;
